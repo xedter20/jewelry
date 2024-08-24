90dcd57d1d47f7faf72db1f33f4443332ec13d7f
xedter20 <dextermiranda441@gmail.com>
2024-08-24 19:34:48 +0800

    chnage secret

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken';
 
-const jwtSecret = 'amulet';
+const jwtSecret = 'secret';
 export const authenticateUserMiddleware = (req, res, next) => {
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
